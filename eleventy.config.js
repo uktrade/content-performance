e0234cd4c68c176ae03a5f9dada006bb302fa391
Stephen Ranford <git@bugs.me.uk>
2024-10-29 16:22:13 +0000

    move pathprefix to build script

--- eleventy.config.js
+++ eleventy.config.js
@@ -34,7 +34,6 @@ module.exports = function(eleventyConfig) {
   
 
   return {
-    pathPrefix: "/content-performance/",
     dataTemplateEngine: 'njk',
     htmlTemplateEngine: 'njk',
     markdownTemplateEngine: 'njk',
